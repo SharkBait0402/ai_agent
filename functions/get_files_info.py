abd09365c555f4624073668a5a8c10ac2fb369d6
Casen Nagy <casen.nagy@gmail.com>
2025-09-06 11:19:13 -0500

    added get_files_info and started creating get_file_content in functions folder

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    file_path = os.path.join(working_directory, directory)
+    dir_path = os.path.abspath(file_path)
+    work_path = os.path.abspath(working_directory)
+    if not dir_path.startswith(work_path):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    all_files = os.listdir(file_path)
+    files = []
+    for file in all_files:
+        if not file.startswith("."):
+            files.append(file)
+
+    tmp_list = []
+    for file in files:
+        active_file = f"{file_path}/{file}"
+        temp = f"- {file}: file_size={os.path.getsize(active_file)}, is_dir={os.path.isdir(active_file)}"
+        tmp_list.append(temp)
+
+    file_str = "\n".join(tmp_list)
+
+    return file_str
--- get_files_info.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def get_files_info(working_directory, directory="."):
-    print(os.path(directory))
-
-print(os.listdir())
