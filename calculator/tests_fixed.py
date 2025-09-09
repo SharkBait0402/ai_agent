0edf4e49fb06ff7fd17aeb14b0b0dcce45a329ee
Casen Nagy <casen.nagy@gmail.com>
2025-09-08 23:45:16 -0500

    removed some files and reverted stuff to how it should be

--- calculator/tests_fixed.py
+++
@@ -1,9 +0,0 @@
-def my_function(x):
-    return x + 1
-
-result = my_function(5)
-
-if result == 6:
-    print("OK")
-else:
-    print("ERROR")
--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -4,11 +4,9 @@ import sys
 from google.genai import types 
 
 def run_python_file(working_directory, file_path, args=[]):
-
     abs_path = os.path.join(working_directory, file_path)
     dir_path = os.path.abspath(abs_path)
     work_path = os.path.abspath(working_directory)
-
     if not dir_path.startswith(work_path):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'    
     elif not os.path.isfile(abs_path):
@@ -16,29 +14,30 @@ def run_python_file(working_directory, file_path, args=[]):
     elif not file_path.endswith(".py"):
         return f'Error: "{file_path}" is not a Python file.'
 
-    try:
-        new_args = ["python", abs_path]
-        if args:
-            new_args.extend(args)
-        
-        obj = subprocess.run(new_args, timeout=30, capture_output=True )
-
-        stdout = (obj.stdout or b"").decode()
-        stderr = (obj.stderr or b"").decode()
-        
-        if obj.returncode != 0:
-            return f"Process exited with code {obj.returncode}"
+    new_args = ["python", abs_path]
+    if args:
+        new_args.extend(args)
     
-        if stdout.strip() == "" and stderr.strip() != "":
-            # some runners print to stderr
-            return f"STDERR: {stderr}"
-        
-        if stdout.strip() == "":
-            return "no output produced"
-    except Exception as e :
-        return "error running python file"
+ 
+
+    obj = subprocess.run(new_args, timeout=30, capture_output=True )
+    
+    stdout = (obj.stdout or b"").decode()
+    stderr = (obj.stderr or b"").decode()
+    
+    if obj.returncode != 0:
+        return f"Process exited with code {obj.returncode}"
+ 
+    if stdout.strip() == "" and stderr.strip() != "":
+        # some runners print to stderr
+        return f"STDERR: {stderr}"
+    
+    if stdout.strip() == "":
+        return "no output produced"
 
     return f"STDOUT: {obj.stdout}\n\n STDERR: {obj.stderr}"
+
+print(run_python_file("calculator", "../main.py"))
 
 schema_run_python_file = types.FunctionDeclaration(
     name="run_python_file",
@@ -50,11 +49,6 @@ schema_run_python_file = types.FunctionDeclaration(
                 type=types.Type.STRING,
                 description="File to run in python",
             ),
-            "args": types.Schema(
-                type=types.Type.STRING,
-                description="optional args to run with file"
-            )
         },
-        required=["file_path"]
     ),
 )
